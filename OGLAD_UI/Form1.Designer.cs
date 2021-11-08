74b47202b3177572f8c102b8fc11868cd07bb5e6
Julian S <saturnojulian@gmail.com>
2021-11-07 16:28:34 -0800

    First Commit

---
+++ OGLAD_UI/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace OGLAD_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gbxView = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cbxParam1 = new System.Windows.Forms.CheckBox();
+            this.cbxParam2 = new System.Windows.Forms.CheckBox();
+            this.cbxParam3 = new System.Windows.Forms.CheckBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gbxView.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(226, 330);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(43, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // gbxView
+            // 
+            this.gbxView.Controls.Add(this.chart1);
+            this.gbxView.Location = new System.Drawing.Point(373, 56);
+            this.gbxView.Name = "gbxView";
+            this.gbxView.Size = new System.Drawing.Size(401, 218);
+            this.gbxView.TabIndex = 5;
+            this.gbxView.TabStop = false;
+            this.gbxView.Text = "View";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtStatus);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(179, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 72);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data View";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(49, 32);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(236, 20);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Location = new System.Drawing.Point(6, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 86);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Run Controls";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(43, 56);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbxParam3);
+            this.groupBox3.Controls.Add(this.cbxParam2);
+            this.groupBox3.Controls.Add(this.cbxParam1);
+            this.groupBox3.Location = new System.Drawing.Point(289, 56);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(87, 218);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Display";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox2);
+            this.groupBox4.Controls.Add(this.groupBox1);
+            this.groupBox4.Location = new System.Drawing.Point(289, 280);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(485, 106);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Controls";
+            // 
+            // cbxParam1
+            // 
+            this.cbxParam1.AutoSize = true;
+            this.cbxParam1.Location = new System.Drawing.Point(6, 30);
+            this.cbxParam1.Name = "cbxParam1";
+            this.cbxParam1.Size = new System.Drawing.Size(62, 17);
+            this.cbxParam1.TabIndex = 0;
+            this.cbxParam1.Text = "Voltage";
+            this.cbxParam1.UseVisualStyleBackColor = true;
+            // 
+            // cbxParam2
+            // 
+            this.cbxParam2.AutoSize = true;
+            this.cbxParam2.Location = new System.Drawing.Point(6, 53);
+            this.cbxParam2.Name = "cbxParam2";
+            this.cbxParam2.Size = new System.Drawing.Size(60, 17);
+            this.cbxParam2.TabIndex = 1;
+            this.cbxParam2.Text = "Current";
+            this.cbxParam2.UseVisualStyleBackColor = true;
+            // 
+            // cbxParam3
+            // 
+            this.cbxParam3.AutoSize = true;
+            this.cbxParam3.Location = new System.Drawing.Point(6, 76);
+            this.cbxParam3.Name = "cbxParam3";
+            this.cbxParam3.Size = new System.Drawing.Size(56, 17);
+            this.cbxParam3.TabIndex = 2;
+            this.cbxParam3.Text = "Power";
+            this.cbxParam3.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 19);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(389, 193);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 407);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.gbxView);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "KWH Load Analyzer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gbxView.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox gbxView;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox cbxParam3;
+        private System.Windows.Forms.CheckBox cbxParam2;
+        private System.Windows.Forms.CheckBox cbxParam1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ OGLAD_UI/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OGLAD_UI
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        bool guiRunning = false;
+        // Create graphics object
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Set the initial state of the control check boxes.
+            txtStatus.Text = "The O-Glad System is not running";
+            cbxParam1.Checked = true;
+            cbxParam2.Checked = false;
+            cbxParam3.Checked = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //upload signal data to grid
+            try
+            {
+                openFileDialog1.ShowDialog();
+                string fn = openFileDialog1.FileName;
+                string readfile = File.ReadAllText(fn);
+                string[] line = readfile.Split('\n');
+                int count = 0;
+                foreach (string str in line[0].Split(','))
+                {
+                    count++;
+                }
+                dataGridView1.ColumnCount = count;
+                foreach (string s1 in readfile.Split('\n'))
+                {
+                    if (s1 != "")
+                        dataGridView1.Rows.Add(s1.Split(','));
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in loading file.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!guiRunning)
+            {
+                // Turn on GUI running flag
+                guiRunning = true;
+                //plot signal data
+                int rowcount = dataGridView1.RowCount - 1;
+
+                if (cbxParam1.Checked)
+                {   // Creating List of point to plot and scaling wave to fit window
+                    double c1, c2;
+                    for (int i = 1; i < rowcount; i++)
+                    {
+                        c1 = Convert.ToDouble(dataGridView1.Rows[i].Cells[0].Value);
+                        c2 = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                        chart1.Series["Series1"].Points.AddXY(c1, c2);
+                    }
+                }
+                if (cbxParam2.Checked)
+                {
+                    double c1, c2;
+                    for (int i = 1; i < rowcount; i++)
+                    {
+                        c1 = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                        c2 = Convert.ToDouble(dataGridView1.Rows[i].Cells[3].Value);
+                        chart1.Series["Series1"].Points.AddXY(c1, c2);
+                    }
+                }
+                if (cbxParam3.Checked)
+                {
+                    double c1, c2;
+                    for (int i = 1; i < rowcount; i++)
+                    {
+                        c1 = Convert.ToDouble(dataGridView1.Rows[i].Cells[4].Value);
+                        c2 = Convert.ToDouble(dataGridView1.Rows[i].Cells[5].Value);
+                        chart1.Series["Series1"].Points.AddXY(c1, c2);
+                    }
+                }
+                txtStatus.Text = "The O-GLAD System is running";
+
+            }
+            else
+            {
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (guiRunning)
+            {
+                guiRunning = false;
+                g.Clear(Color.White);
+                g.Dispose();
+                txtStatus.Text = " The O-GLAD System is not running";
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
